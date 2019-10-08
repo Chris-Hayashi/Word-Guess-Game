8b4e3602e84de7adbc20ff5a7e77f63c337f2703
Christopher Hayashi <Chrisrhayashi@gmail.com>
2019-10-08 08:10:03 -0700

    created replaceAt() function, but does not work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,24 @@
 var userInput;
-var winPointer = document.getElementById("wins");
-var wordPointer = document.getElementById("word");
-var lettersGuessedPOinter = document.getElementById("lettersGuessed");
+
+function replaceAt(index, replacement) {
+    var wordPointer = document.getElementById("word");
+    word = wordPointer.textContent;
+    word[index] = replacement;
+    console.log(word);
+    wordPointer.textContent = word;
+    console.log("function is called");
+
+}
 
 document.onkeyup = function(event) {
     userInput = event.key;
 
+    var winPointer = document.getElementById("wins");
+
+    var lettersGuessedPOinter = document.getElementById("lettersGuessed");
+
     if (userInput === 'n') {
-        wordPointer.textContent = wordPointer.replace([0], n);
+        replaceAt(0, "n");
+        console.log("n key pressed...");
     }
 }
